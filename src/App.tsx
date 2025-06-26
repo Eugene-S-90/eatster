182729845e3d3eed2f4444ad916bc4b82ab070fc
Eugene Sokolianskyi <eugenesokol04@gmail.com>
2025-06-26 12:57:22 +0300

    add not found page,ref

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,32 @@ import { useRestaurantStore } from './store/useRestorauntStore'
 import { Header } from './components/Header'
 import { CategoryTabs } from './components/CategoryTabs'
 import { CategorySection } from './components/CategorySection'
-import RestarauntLayout from './layout/RestarauntLayout'
+import RestarauntLayout from './layouts/RestarauntLayout'
 import ScrollUpButton from './components/ScrollUpButton'
 import { MealDialog } from './components/MealDialog'
 import { CartBar } from './components/CartBar'
+import { queryParamsParser } from './lib/utils'
+import NotFound from './layouts/NotFound'
 
 function App() {
-  const { fetchMeals } = useRestaurantStore();
+  const { fetchMeals, restaurantError } = useRestaurantStore();
   const fetched = useRef(false)
 
   useEffect(() => {
     if (fetched.current) return
-    fetched.current = true
-    fetchMeals(287);
-  }, [fetchMeals])
+    fetched.current = true;
+    let id = queryParamsParser('id')
+    if (id) {
+      fetchMeals(+id);
+    } else {
+      fetchMeals(287);
+    }
+  }, [fetchMeals]);
+
+  if (restaurantError) {
+    return <NotFound restaurantError={restaurantError} />
+  }
+
   return (
     <RestarauntLayout>
       <Header />
